8523bc7dc040c4d8cf88ce4af4b8c56a4efde930
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-02-15 14:27:41 -0500

    Added Documentation and created a vectorfiller() for the DirectoryToArray class. (still untested)
    Signed-off-by: shrimpboyho <shrimpboyhoftw@gmail.com>

--- DirectoryToArray.h
+++ DirectoryToArray.h
@@ -1,3 +1,31 @@
+/**
+Author: shrimpboyho
+Created on: January 2, 2013
+Copyright 2013
+==============================================
+
+This class has one constructor that only only accepts a string:
+
+DirectoryToArray::DirectoryToArray(path_of_the_directory)
+
+The string is the path to the directory. (Note: all dashes in the path must be done with �/� or �\\� .
+
+==============================================
+
+This class has only 2 methods, which accepts an address to an array of strings or a vector of strings:
+
+DirectoryToArray::arrayfiller(address_of_array_of_strings);
+DirectoryToArray::vectorfiller(address_of_vector);
+
+After this method has run the empty array/vector will be filled with the files in the directory.
+
+==============================================
+*/
+
+
+
+
+
 #ifndef _DirectoryToArray
 #define _DirectoryToArray
 
@@ -7,6 +35,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <vector>
 
 using namespace std;
 class DirectoryToArray
@@ -16,6 +45,7 @@ public:
 
     string directorypath;
     void arrayfiller(string*);
+    void vectorfiller(vector<string>*);
     DirectoryToArray(string);
 
 
@@ -30,7 +60,8 @@ void DirectoryToArray::arrayfiller(string* arraypointer)
     dp = opendir (chardirectorypath);
     if (dp != NULL)
     {
-        while (ep = readdir (dp)){
+        while (ep = readdir (dp))
+        {
             *arraypointer=(string)ep->d_name;
             arraypointer++;
         }
@@ -41,7 +72,32 @@ void DirectoryToArray::arrayfiller(string* arraypointer)
 
 
 }
-DirectoryToArray::DirectoryToArray(string given){
+DirectoryToArray::DirectoryToArray(string given)
+{
     directorypath = given;
+}
+
+void DirectoryToArray::vectorfiller(vector<string>*givenpointer)
+{
+
+    char *chardirectorypath = (char*)directorypath.c_str();
+    DIR *dp;
+    struct dirent *ep;
+
+    dp = opendir (chardirectorypath);
+    if (dp != NULL)
+    {
+        while (ep = readdir (dp))
+        {
+            givenpointer->push_back((string)ep->d_name);
+            givenpointer++;
+        }
+        (void) closedir (dp);
+    }
+    else
+        perror ("Couldn't open the directory");
+
+
+
 }
 #endif
