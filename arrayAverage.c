f54f2b2849a238afe558e1c7e2aad00ecf065f14
dewey <new.joseph10@gmail.com>
2013-02-15 09:58:14 -0500

    Fixed some logic

--- arrayAverage.c
+++ arrayAverage.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-void averageArray(int *givenpassedpointer, int givenfinalsize);
+
+double averageArray(int *givenpassedpointer, int givenfinalsize);
 
 
 
-void averageArray(int *givenpassedpointer, int givenfinalsize){
+double averageArray(int *givenpassedpointer, int givenfinalsize){
 int counter = 0;
 int sum = 0;
 while(counter <= finalsize){
@@ -15,10 +16,9 @@ while(counter <= finalsize){
 
 
 }
-printf("\n============================================================");
-printf(" \nThe sum is %d \n \n", sum);
+
 double answer =  ((double)sum/(finalsize+1));
-double average = answer/givenfinalsize;
-printf("The average is %g \n", answer);
+
+return answer;
 }
 
